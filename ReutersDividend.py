ad43c11fe081c7384c3ad084008c0063f036db9b
AtToftegaard <Andreas@toftegaard.org>
2017-09-30 13:24:30 +0200

    Collect dividends where possible and store in dict with name

--- ReutersDividend.py
+++ ReutersDividend.py
@@ -4,46 +4,60 @@ import urllib3
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from requests import get
+import csv
 import google
 
-stockurl = "https://npinvestor.dk/aktier-og-kurslister/aktier/danmark/alle-danske-aktier"
+# Take ticker, return url for company-page
+def getURLforPage(ticker):
+    url = "http://www.reuters.com/finance/stocks/lookup?searchType=any&comSortBy=marketcap&sortBy=&dateRange=&search=" + ticker
+    response = get(url)
+    search_soup = BeautifulSoup(response.text, 'html.parser')
+    partURL = search_soup.find('tr', class_='stripe').get('onclick')[17:-1]
+    return ("http://www.reuters.com"+partURL)
+
+    
+# Take stockticker, return dividend
+def findDividend(ticker):
+    url = getURLforPage(ticker)
+    response = get(url)
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    
+    keywords = ['Yield']
+    for tr in html_soup.find_all('tr'):
+        string = tr.get_text()
+        if any(word in string for word in keywords):
+            return(string);
+
+
+stockurl = "http://www.nasdaqomxnordic.com/shares/listed-companies/copenhagen"
 
 response = get(stockurl)
 stockpage_soup = BeautifulSoup(response.text, 'html.parser')
+table = stockpage_soup.find('table', id = "listedCompanies")
+headings = [header.get_text() for header in stockpage_soup.find('tr').find_all('th')]
 
-danskeAktier = stockpage_soup.find_all('div', class_="table-row-even float-columns")
-stockList = []
-URLlist = []
-
-for stock in danskeAktier:
-    name = stock.div.a
-    name = name.text
-    stockList.append(name)
-
-for stockName in stockList:
-    for url in google.search((stockName + 'Reuters'), num = 4, stop = 1, pause = 2.0):
-        url.text
-        if url.endswith('.CO'):
-            URLlist.append(url)
+yield_dict = {}
+datasets= []
+for row in table.find_all('tr')[1:]:
+    dataset = zip(headings, (td.get_text() for td in row.find_all('td')))
+    datasets.append(dataset)
 
 
-#for url in google.search(stockName, num = 1, stop = 1):
+for dataset in datasets:
+    for field in dataset:
+        if field[0] == 'Name':
+            companyname = field[1]
+        if field[0] == 'Symbol': 
+            #print('finding for '+ field[1])
+            yield_dict[companyname] = findDividend(field[1])
+            
+for key in yield_dict:
+    print( key, yield_dict[key]) 
+                
+       # print ("{0:<16}: {1}".format(field[0], field[1]))
 
 
 
-# =============================================================================
-# 
-# url = "http://mobile.reuters.com/finance/stocks/overview/MATAS.CO"
-# 
-# response = get(url)
-# html_soup = BeautifulSoup(response.text, 'html.parser')
-#  
-# div_containers = html_soup.find_all('div', class_="section-quote-detail group")
-# 
-# for div in div_containers:
-#     string = div.text
-#     if 'Dividend' in string:
-#         string = " ".join(string.split())
-#         print(string)
-# 
-# =============================================================================
+
+
+
